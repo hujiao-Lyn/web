b343a20fc355716999e26f318bc7c6b5d0d901c8
hujiao-Lyn <1445120694@qq.com>
2020-12-13 21:53:40 +0800

    JS_day10

---
+++ json.js
@@ -0,0 +1,23 @@
+const oStudent={
+    studentName:"hujiao",
+    age:20,
+    len:null,
+    len1:undefined,
+    org:['love','smart'],
+    course:{name:'hu',day:1},
+    learn:function(){
+        console.log("ai bb")
+    }
+}
+let a = JSON.stringify(oStudent)
+console.log(a)
+
+const b = `{'studentName':"hujiao","age":20,"len":null,"org":["love","smart"],"course":{"name":
+"ww","day":1}}`
+const a = JSON.parse(b)
+console.log(a)
+
+
+for (let p in a){
+    console.log(p)
+}
